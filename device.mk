5b3a6e22d72872be60e4c123d247a98acd21cc69
julival25 <julival.bitencourt@gmail.com>
2022-03-11 12:48:04 -0300

    Polaris: setting Firmware as optional

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 $(call inherit-product-if-exists, vendor/xiaomi/polaris/polaris-vendor.mk)
 
 # Firmware
-$(call inherit-product, vendor/xiaomi-firmware/polaris/firmware.mk)
+$(call inherit-product-if-exists, vendor/xiaomi-firmware/polaris/firmware.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
