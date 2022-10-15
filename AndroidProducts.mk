630758ea39c08e64ecfac4b6af8a7f820ca3c8db
bittencourtjulival <julival.bitencourt@gmail.com>
2022-10-15 13:27:24 -0300

    Polaris: bringup SparkOS Android 13

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_polaris.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_polaris-user \
-    lineage_polaris-userdebug \
-    lineage_polaris-eng
+    spark_polaris-user \
+    spark_polaris-userdebug \
+    spark_polaris-eng
--- spark_polaris.mk
+++ spark_polaris.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common spark stuff.
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_polaris
+PRODUCT_NAME := spark_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
