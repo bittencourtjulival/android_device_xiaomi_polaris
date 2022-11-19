6384a88d867cb02375ab083f4660559fcfbe7e3c
Bruno Martins <bgcngm@gmail.com>
2022-11-19 06:19:39 -0300

    polaris: Drop updatable_apex inclusion and enable compression

--- device.mk
+++ device.mk
@@ -9,11 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Firmware
 $(call inherit-product-if-exists, vendor/xiaomi-firmware/polaris/firmware.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-PRODUCT_COMPRESSED_APEX := false
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/polaris/polaris-vendor.mk)
 
