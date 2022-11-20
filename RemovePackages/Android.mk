2889f83b3e76d68ad5a72aaf9902bfb01d088dc8
bittencourtjulival <julival.bitencourt@gmail.com>
2022-11-20 11:09:59 -0300

    Polaris: update RemovePackages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,8 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Chrome Chrome-Stub DevicePolicyPrebuilt Drive GoogleCamera Maps MyVerizonServices OPScreenRecord Ornament OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt Snap YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger WallpapersBReel2020 obdm_stub Videos AndroidAutoStubPrebuilt
-LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub AndroidAutoStubPrebuilt Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore GoogleFeedback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
