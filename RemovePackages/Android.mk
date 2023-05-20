999a10c857f90fe47dc2059f78e4ba63b96f248d
bittencourtjulival <julival.bitencourt@gmail.com>
2023-05-20 13:01:53 -0300

    polaris: update remove_packages | Fix Android Auto error

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub AndroidAutoStubPrebuilt Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore GoogleFeedback
+LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore GoogleFeedback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
