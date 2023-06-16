c611b01fc0d1191634517de40a60a63d7e26c5ad
bittencourtjulival <julival.bitencourt@gmail.com>
2023-06-15 21:35:07 -0300

    Revert "Polaris: Cleanup lineage stuff"
    This reverts commit 3078718069b9594d5cc13dc069b3d6db0ebe2b9c.

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
